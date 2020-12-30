c3fad720664c91384227182a877d34a8ba7fefaa
Dave Herzog <davidjamesherzog@users.noreply.github.com>
2020-12-30 16:17:58 -0400

    Typescript decorators example (#1)

---
+++ src/components/Counter.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <button @click="add">+</button>
+      <button @click="subtract">-</button>
+    </div>
+    <div>State: {{ counter }}</div>
+    <div>Getter: {{ getCounter }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import { namespace } from "vuex-class";
+
+const counterModule = namespace("counter");
+
+@Options({
+  name: "Counter",
+  props: {
+    msg: String
+  }
+})
+export default class Counter extends Vue {
+  @Prop({ type: String })
+  private msg!: string;
+
+  @counterModule.State("_count")
+  private counter!: number;
+
+  @counterModule.Getter("count")
+  private getCounter: any;
+
+  @counterModule.Action
+  private add: any;
+
+  @counterModule.Action
+  private subtract: any;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <Counter msg="Counter" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
@@ -8,10 +9,13 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Counter from "@/components/Counter.vue";
 
 @Options({
+  name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Counter
   }
 })
 export default class Home extends Vue {}
